c1a87db08fd914cb20bd0c14c0f33c1c413f6730
toothgrinder <johnwilfer.uson@Outlook.com>
2018-01-26 23:54:52 +0800

    added zombie

---
+++ src/ggj_game/zombie/Zombie_A.java
@@ -0,0 +1,11 @@
+package ggj_game.zombie;
+
+/*Zombie Type */
+public class Zombie_A {
+	public static final int CONEHEAD = 777;
+	public static final int POLICE = 778;
+	public static final int SPIKED = 779;
+	public static final int FOOTBALL = 780;
+	public static final int BLOAT = 781;
+	public static final int KID = 782;
+}
---
+++ src/ggj_game/zombie/Zombie_U.java
@@ -0,0 +1,7 @@
+package ggj_game.zombie;
+
+/* UPGRADES */
+
+public class Zombie_U {
+	public static final int DECAY = 0;
+}
