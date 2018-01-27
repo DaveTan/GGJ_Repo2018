be60b1f90705264ea71cc57a6e583d4c8814a6bb
DavusTanus <ChristianDave.Tan@gmail.com>
2018-01-27 10:42:50 +0800

    added spawning

--- src/ggj_game/states/test/Test_P.java
+++ src/ggj_game/states/test/Test_P.java
@@ -1,10 +1,15 @@
 package ggj_game.states.test;
 
+import ggj_game.entities.Entities_P;
+import ggj_game.entities.Test_Entity_P;
+import ggj_game.input.Keyboard_P;
 import ggj_game.utils.ImageRes;
 import ggj_game.utils.game_map.GameMap;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.MouseListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -12,7 +17,7 @@ import org.newdawn.slick.state.StateBasedGame;
 /**
  * Created by Clientrace on 26/01/2018.
  */
-public class Test_P extends BasicGameState{
+public class Test_P extends BasicGameState implements MouseListener{
 
     @Override
     public int getID() {
@@ -23,18 +28,28 @@ public class Test_P extends BasicGameState{
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         Test_V.initialize();
         ImageRes.init();
-        Test_V.entity.setDest(5,5);
+//        Test_V.entity.setDest(5,5);
+        Entities_P.add(new Test_Entity_P(3, 3));
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sb, Graphics g) throws SlickException {
         Test_V.gameMap1.render(g,0,0);
-        g.setColor(new Color(255,255,255));
-        g.fillRect(Test_V.entity.getX(),Test_V.entity.getY(),32,32);
+//        g.setColor(new Color(255,255,255));
+//        g.fillRect(Test_V.entity.getX(),Test_V.entity.getY(),32,32);
+//        Test_V.entity.render();
+        Entities_P.draw();
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sb, int i) throws SlickException {
-        Test_V.entity.update();
+//        Test_V.entity.update(i);
+    	Entities_P.update(i);
+    }
+    
+    @Override
+    public void mousePressed(int button, int x, int y){
+    	System.out.println(Thread.currentThread().getStackTrace()[1]);
+    	Entities_P.add(new Test_Entity_P(x, y));
     }
 }
--- src/ggj_game/states/test/Test_V.java
+++ src/ggj_game/states/test/Test_V.java
@@ -1,6 +1,7 @@
 package ggj_game.states.test;
 
 import ggj_game.entities.Object;
+import ggj_game.entities.Test_Entity_P;
 import ggj_game.utils.game_map.GameMap;
 
 /**
@@ -9,10 +10,10 @@ import ggj_game.utils.game_map.GameMap;
 public class Test_V {
     public static int TILE_SIZE = 32;
     public static GameMap gameMap1;
-    public static Object entity;
+    public static Test_Entity_P entity;
 
     public static void initialize(){
         gameMap1 = new GameMap("res/maps/main_map.json",TILE_SIZE);
-        entity = new Object();
+        entity = new Test_Entity_P(3, 3);
     }
 }
